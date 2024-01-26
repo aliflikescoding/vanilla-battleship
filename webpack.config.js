4ea3b8b591fbb2548c36c1e877606bfb5b788b9a
aliflikescoding <alif.likes.coding@gmail.com>
2024-01-26 17:55:57 +0700

    SASS setup

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,18 @@ const path = require('path');
 module.exports = {
   plugins: [],
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+    ]
+  },
   entry: './src/index.js',
   output: {
     filename: 'main.js',
