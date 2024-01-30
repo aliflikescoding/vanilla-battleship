fc84d423ac815c90dbbd6c288b9bf09664a40c86
aliflikescoding <alif.likes.coding@gmail.com>
2024-01-30 09:43:29 +0700

    Add ship_2 class and it's test unit

---
+++ src/_ship_2.js
@@ -0,0 +1,10 @@
+const Ship = require('./_ship');
+
+class Ship_2 extends Ship {
+  constructor() {
+    super(); 
+    this.length = 2;
+  }
+}
+
+module.exports = Ship_2
---
+++ src/tests/ship_2.test.js
@@ -0,0 +1,29 @@
+const Ship_2 = require('../_ship_2');
+
+test("get ship length", () => {
+  const ship = new Ship_2();
+
+  expect(ship.getLength()).toBe(2);
+});
+
+test("get ships hit", () => {
+  const ship = new Ship_2();
+
+  expect(ship.getHits()).toBe(0);
+});
+
+test("test the hitShip function", () => {
+  const ship = new Ship_2();
+  ship.hitShip();
+  ship.hitShip();
+
+  expect(ship.getHits()).toBe(2);
+});
+
+test("test isSunk function", () => {
+  const ship = new Ship_2();
+  ship.hitShip();
+  ship.hitShip();
+
+  expect(ship.isSunk()).toBe(true);
+});
