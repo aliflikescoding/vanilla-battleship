5c2edc6d9f2a378cf52431c8f339fef03b82120b
aliflikescoding <alif.likes.coding@gmail.com>
2024-02-03 21:12:16 +0700

    Make place vertical and horizontal factory functions

--- src/_gameBoard.js
+++ src/_gameBoard.js
@@ -15,17 +15,52 @@ function GameBoard() {
   this.shipArray = [];
 }
 
-GameBoard.prototype.rotateShipPosition() = function () {
+// getters
+GameBoard.prototype.getRotate = function () {
+  return this.rotate;
+};
+
+GameBoard.prototype.getGrid = function () {
+  return this.grid;
+};
+
+// setters
+GameBoard.prototype.setGrid = function (grid) {
+  return this.grid = grid;
+};
+
+GameBoard.prototype.rotateShipPosition = function () {
   if (this.rotate === false) {
     this.rotate = true;
   }
   else {
     this.rotate = false;
   }
-}
+};
 
-GameBoard.prototype.getRotateStatus() = function () {
-  return this.rotate;
-}
+GameBoard.prototype.placeHorizontal = function (Xcoordinates, Ycoordinates, shipType) {
+  let array = this.getGrid();
+  for (let i = 0; i <= shipType-1; i++) {
+    array[Ycoordinates][Xcoordinates+i] = 1;
+  }
+  this.setGrid(array);
+};
+
+GameBoard.prototype.placeVertical = function (Xcoordinates, Ycoordinates, shipType) {
+  let array = this.getGrid();
+  for (let i = 0; i <= shipType-1; i++) {
+    array[Ycoordinates+i][Xcoordinates] = 1;
+  }
+  this.setGrid(array);
+};
+
+GameBoard.prototype.placeShip = function (Xcoordinates, Ycoordinates, incrementNumber, shipType) {
+  if (this.rotate == false) {
+    this.placeHorizontal(Xcoordinates, Ycoordinates, shipType);
+  }
+  else {
+    this.placeHorizontal(Xcoordinates, Ycoordinates, shipType);
+  }
+};
 
 module.exports = GameBoard
