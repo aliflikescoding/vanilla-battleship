c25a7c4217d746e101dfd9e5b3c49d380874d050
aliflikescoding <alif.likes.coding@gmail.com>
2024-02-14 23:44:04 +0700

    Add pick module and imported it to main and Add pick button classes and selected them in the pick module

---
+++ src/modules/_pick.js
@@ -0,0 +1,4 @@
+// pick buttons
+const buttonRotate = document.querySelector("#buttonRotate");
+const buttonStartGame = document.querySelector("#buttonStartGame");
+const buttonUndo = document.querySelector("#buttonUndo");
