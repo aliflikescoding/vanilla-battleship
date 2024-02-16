228c1d7e1fc0091cf608d28aee77af82b5d16d79
aliflikescoding <alif.likes.coding@gmail.com>
2024-02-17 01:56:27 +0700

    Add checkPlacement hover effect

--- src/modules/_game.js
+++ src/modules/_game.js
@@ -21,45 +21,87 @@ pickGrids.forEach((grid) => {
   grid.addEventListener("mouseenter", () => {
     const gridPosition = grid.classList[1];
     const classNames = pick.returnGridClasses(pick.type(playerBoard.getShipNumber()), playerBoard.getRotate(), gridPosition);
+    const placementStatus = pick.checkPlacement(classNames);
     const outOfBoundsStatus = pick.checkOutOfBounds(gridPosition, playerBoard.getRotate(), pick.type(playerBoard.getShipNumber()));
     const popNum = pick.getPopNumber(gridPosition, pick.type(playerBoard.getShipNumber()), playerBoard.getRotate());
     if (outOfBoundsStatus == false) {
-      classNames.forEach((className) => {
-        document.querySelector(`.${className}`).classList.add("pick-color");
-      });
-      grid.classList.add("pick-color");
-    } else {
-      for (let i = 0; i < popNum; i++) {
-        classNames.pop();
-      }
-      if (classNames.length > 0) {
+      if (placementStatus == false) {
         classNames.forEach((className) => {
-          document.querySelector(`.${className}`).classList.add("pick-danger");
+          if (document.querySelector(`.${className}`) !== null) {
+            document.querySelector(`.${className}`).classList.add("pick-danger");
+          }
         });
+        grid.classList.add("pick-danger");
       }
-      grid.classList.add("pick-danger");
+      else {
+        classNames.forEach((className) => {
+          document.querySelector(`.${className}`).classList.add("pick-color");
+        });
+        grid.classList.add("pick-color");
+      }
+    } else {
+      if (placementStatus == false) {
+        classNames.forEach((className) => {
+          if (document.querySelector(`.${className}`) !== null) {
+            document.querySelector(`.${className}`).classList.add("pick-danger");
+          }
+        });
+        grid.classList.add("pick-danger");
+      }
+      else {
+        for (let i = 0; i < popNum; i++) {
+          classNames.pop();
+        }
+        if (classNames.length > 0) {
+          classNames.forEach((className) => {
+            document.querySelector(`.${className}`).classList.add("pick-danger");
+          });
+        }
+        grid.classList.add("pick-danger");
+      }
     }
   });
   grid.addEventListener("mouseleave", () => {
     const gridPosition = grid.classList[1];
     const classNames = pick.returnGridClasses(pick.type(playerBoard.getShipNumber()), playerBoard.getRotate(), gridPosition);
+    const placementStatus = pick.checkPlacement(classNames);
     const outOfBoundsStatus = pick.checkOutOfBounds(gridPosition, playerBoard.getRotate(), pick.type(playerBoard.getShipNumber()));
     const popNum = pick.getPopNumber(gridPosition, pick.type(playerBoard.getShipNumber()), playerBoard.getRotate());
     if (outOfBoundsStatus == false) {
-      classNames.forEach((className) => {
-        document.querySelector(`.${className}`).classList.remove("pick-color");
-      });
-      grid.classList.remove("pick-color");
-    } else {
-      for (let i = 0; i < popNum; i++) {
-        classNames.pop();
-      }
-      if (classNames.length > 0) {
+      if (placementStatus == false) {
         classNames.forEach((className) => {
-          document.querySelector(`.${className}`).classList.remove("pick-danger");
+          if (document.querySelector(`.${className}`) !== null) {
+            document.querySelector(`.${className}`).classList.remove("pick-danger");
+          }
         });
+        grid.classList.remove("pick-danger");
       }
-      grid.classList.remove("pick-danger");
+      else {
+        classNames.forEach((className) => {
+          document.querySelector(`.${className}`).classList.remove("pick-color");
+        });
+        grid.classList.remove("pick-color");
+      }
+    } else {
+      if (placementStatus == false) {
+        classNames.forEach((className) => {
+          if (document.querySelector(`.${className}`) !== null) {
+            document.querySelector(`.${className}`).classList.remove("pick-danger");
+          }
+        });
+        grid.classList.remove("pick-danger");
+      }
+      else {
+        for (let i = 0; i < popNum; i++) {
+          classNames.pop();
+        }
+        if (classNames.length > 0) {
+          classNames.forEach((className) => {
+            document.querySelector(`.${className}`).classList.remove("pick-danger");
+          });
+        }
+        grid.classList.remove("pick-danger");
+      }
     }
   });
   grid.addEventListener("click", () => {
