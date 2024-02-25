ce66a2d50b1c07db2ee6bbaba50fb3bd705e57e6
aliflikescoding <alif.likes.coding@gmail.com>
2024-02-26 00:58:38 +0700

    Add bot basics

---
+++ src/objects/_bot.js
@@ -0,0 +1,27 @@
+function  Bot() {
+  this.type = 0;
+  this.grid = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ];
+}
+
+Bot.prototype.getRandomNumber = function () {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+Bot.prototype.reserveSpot = function () {
+  const x = this.prototype.getRandomNumber();
+  const y = this.prototype.getRandomNumber();
+
+}
+
+module.exports = Ship
