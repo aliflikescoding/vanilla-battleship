6f84892ec68851fba8ef627cc3b315f6f96b4491
aliflikescoding <alif.likes.coding@gmail.com>
2024-02-27 18:31:22 +0700

    Get reserve spot function

--- src/objects/_bot.js
+++ src/objects/_bot.js
@@ -38,6 +38,28 @@ Bot.prototype.getRotateStatus = function () {
   return Math.floor(Math.random() * 2);
 }
 
+Bot.prototype.reserveSpot = function (rotateStatus, x, y) {
+  const grid = this.getGrid();
+  if (rotateStatus === 0 || rotateStatus === 1) {
+    for (let i = 0; i < 4; i++) {
+      grid[y][x] = 1;
+    }
+  } else if (rotateStatus === 2 || rotateStatus === 3) {
+    for (let i = 0; i < 3; i++) {
+      grid[y][x] = 1;
+    }
+  }
+  else if (rotateStatus === 2 || rotateStatus === 3) {
+    for (let i = 0; i < 3; i++) {
+      grid[y][x] = 1;
+    }
+  } else {
+    for (let i = 0; i < 2; i++) {
+      grid[y][x] = 1;
+    }
+  }
+}
+
 Bot.prototype.getSpots = function () {
   let arr = [];
   const rotateStatus = this.getRotateStatus();
