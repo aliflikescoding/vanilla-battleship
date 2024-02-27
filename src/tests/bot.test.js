712640db86d2f314c91e48dff5227a4475384db5
aliflikescoding <alif.likes.coding@gmail.com>
2024-02-27 19:03:06 +0700

    Add check status test

--- src/tests/bot.test.js
+++ src/tests/bot.test.js
@@ -1,4 +1,5 @@
-const Bot = require('../objects/_bot');
+const { experiments } = require("webpack");
+const Bot = require("../objects/_bot");
 
 test("reserving spots 1", () => {
   const bot = new Bot();
@@ -36,7 +37,6 @@ test("reserving spots 2", () => {
   ]);
 });
 
-
 test("reserving spots 3", () => {
   const bot = new Bot();
   bot.reserveSpot(0, 3, 1, 2);
@@ -71,4 +71,25 @@ test("reserving spots 3", () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+});
+
+test("check status 1", () => {
+  const bot = new Bot();
+  bot.reserveSpot(0, 0, 1, 0);
+
+  expect(bot.checkSpot(0, 0, 1, 0)).toBe(false);
+});
+
+test("check status 2", () => {
+  const bot = new Bot();
+  bot.reserveSpot(0, 4, 1, 2);
+
+  expect(bot.checkSpot(0, 4, 0, 2)).toBe(false);
+});
+
+test("check status 3", () => {
+  const bot = new Bot();
+  bot.reserveSpot(0, 4, 1, 2);
+
+  expect(bot.checkSpot(0, 4, 0, 0)).toBe(true);
 });
