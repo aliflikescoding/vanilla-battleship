acaf295da57607fb1ca79a9be18e1b903d6c5ac6
aliflikescoding <alif.likes.coding@gmail.com>
2024-02-27 20:42:24 +0700

    Finish get spots function

--- src/objects/_bot.js
+++ src/objects/_bot.js
@@ -1,4 +1,4 @@
-function  Bot() {
+function Bot() {
   this.type = 0;
   this.grid = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -14,64 +14,63 @@ function  Bot() {
   ];
 }
 
-Bot.prototype.getGrid = function () {
-  return this.grid;
-}
-
-Bot.prototype.getType = function () {
-  return this.type;
-}
-
-Bot.prototype.setGrid = function (newGrid) {
-  this.grid = newGrid;
-}
-
-Bot.prototype.incrementType = function () {
-  this.type += 1;
-}
-
 Bot.prototype.getRandomNumber = function () {
   return Math.floor(Math.random() * 6) + 1;
 }
 
+Bot.prototype.getGrid = function () {
+  return this.grid;
+};
+
+Bot.prototype.getType = function () {
+  return this.type;
+};
+
+Bot.prototype.setGrid = function (newGrid) {
+  this.grid = newGrid;
+};
+
+Bot.prototype.incrementType = function () {
+  this.type += 1;
+};
+
 Bot.prototype.getRotateStatus = function () {
-  return Math.floor(Math.random() * 2);
-}
+  return Math.random() >= 0.5 ? 1 : 0;
+};
 
 Bot.prototype.reserveSpot = function (rotateStatus, type, x, y) {
   const grid = this.getGrid();
   if (rotateStatus == 0) {
     if (type === 0 || type === 1) {
       for (let i = 0; i < 4; i++) {
-        grid[y][x+i] = 1;
+        grid[y][x + i] = 1;
       }
     } else if (type === 2 || type === 3) {
       for (let i = 0; i < 3; i++) {
-        grid[y][x+i] = 1;
+        grid[y][x + i] = 1;
       }
     } else {
       for (let i = 0; i < 2; i++) {
-        grid[y][x+i] = 1;
+        grid[y][x + i] = 1;
       }
     }
-  }
-  else {
+  } else {
     if (type === 0 || type === 1) {
       for (let i = 0; i < 4; i++) {
-        grid[y+i][x] = 1;
+        grid[y + i][x] = 1;
       }
     } else if (type === 2 || type === 3) {
       for (let i = 0; i < 3; i++) {
-        grid[y+i][x] = 1;
+        grid[y + i][x] = 1;
       }
     } else {
       for (let i = 0; i < 2; i++) {
-        grid[y+i][x] = 1;
+        grid[y + i][x] = 1;
       }
     }
   }
   this.setGrid(grid);
-}
+};
 
 Bot.prototype.checkSpot = function (rotateStatus, type, x, y) {
   const grid = this.getGrid();
@@ -79,67 +78,70 @@ Bot.prototype.checkSpot = function (rotateStatus, type, x, y) {
   if (rotateStatus == 0) {
     if (type === 0 || type === 1) {
       for (let i = 0; i < 4; i++) {
-        if (grid[y][x+i] === 1) {
+        if (grid[y][x + i] === 1) {
           status = false;
         }
       }
     } else if (type === 2 || type === 3) {
       for (let i = 0; i < 3; i++) {
-        if (grid[y][x+i] === 1) {
+        if (grid[y][x + i] === 1) {
           status = false;
         }
       }
     } else {
       for (let i = 0; i < 2; i++) {
-        if (grid[y][x+i] === 1) {
+        if (grid[y][x + i] === 1) {
           status = false;
         }
       }
     }
-  }
-  else {
+  } else {
     if (type === 0 || type === 1) {
       for (let i = 0; i < 4; i++) {
-        if (grid[y+i][x] === 1) {
+        if (grid[y + i][x] === 1) {
           status = false;
         }
       }
     } else if (type === 2 || type === 3) {
       for (let i = 0; i < 3; i++) {
-        if (grid[y+i][x] === 1) {
+        if (grid[y + i][x] === 1) {
           status = false;
         }
       }
     } else {
       for (let i = 0; i < 2; i++) {
-        if (grid[y+i][x] === 1) {
+        if (grid[y + i][x] === 1) {
           status = false;
         }
       }
     }
   }
   return status;
-}
+};
 
-Bot.prototype.getSpot = function () {
+Bot.prototype.getSpot = function (rttstatus) {
   const x = this.getRandomNumber();
   const y = this.getRandomNumber();
-  const rotateStatus = this.getRotateStatus();
   const type = this.getType();
-  let arr = [];
-  if (this.checkSpot(rotateStatus, type, x, y) === true) {
-    arr.push(x);
-    arr.push(y);
-    return arr;
+  if (this.checkSpot(rttstatus, type, x, y) === true) {
+    return [x, y, rttstatus];
+  } else {
+    return this.getSpot(rttstatus);
   }
-  else {
-    this.getSpot();
-  }
-}
+};
 
 Bot.prototype.getSpots = function () {
   let arr = [];
-  const rotateStatus = this.getRotateStatus();
-}
+  for (let i = 1; i <= 5; i++) {
+    const rt = this.getRotateStatus();
+    const positions = this.getSpot(rt);
+    const x = positions[0];
+    const y = positions[1];
+    this.reserveSpot(rt, this.getType(), x, y);
+    this.incrementType();
+    arr.push(positions);
+  }
+  return arr;
+};
 
-module.exports = Bot
+module.exports = Bot;
